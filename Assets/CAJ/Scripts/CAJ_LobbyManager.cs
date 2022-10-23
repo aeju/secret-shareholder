6d37a52a229d4a8160bacc31806dc375fb1d55ff
aeju <aejuchoe@naver.com>
2022-10-23 20:02:37 +0900

    1023 / 방 생성 o

--- Assets/CAJ/Scripts/CAJ_LobbyManager.cs
+++ Assets/CAJ/Scripts/CAJ_LobbyManager.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CAJ_LobbyManager : MonoBehaviourPunCallbacks
 {
-    //1. 방 생성 (지금 : 시작하자마자, 나중엔 고치기)
-    // Start is called before the first frame update
+    //방 이름, 총 인원 -> 방 참가 되도록
+    //방이름 InputField
+    public InputField inputRoomName;
+    //총인원 InputField
+    public InputField inputMaxPlayer;
+    //방참가 Button
+    public Button btnJoin;
+    //방생성 Button
+    public Button btnCreate;
+
     void Start()
     {
-        CreateRoom();
+        //CreateRoom();
+        //방 이름이 변할 때마다 호출하게 하는 함수 등록
+        inputRoomName.onValueChanged.AddListener(OnRoomNameValueChanged);
+        //총 인원이 변할 때마다 호출하게 하는 함수 등록
+        inputMaxPlayer.onValueChanged.AddListener(OnMaxPlayerValueChanged);
     }
+    
+    private void OnRoomNameValueChanged(string room)
+    {
+        //생성 버튼 활성화
+        btnCreate.interactable = room.Length > 0;
+        print("방 이름 입력 : " + room);
+        //참여 버튼 활성화
+        btnJoin.interactable = room.Length > 0 && inputMaxPlayer.text.Length > 0;
+    }
+
+
+    private void OnMaxPlayerValueChanged(string max)
+    {
+        btnCreate.interactable = max.Length > 0 && inputRoomName.text.Length > 0;
+    }
+    
 
     //방 생성
     public void CreateRoom()
@@ -20,13 +49,14 @@ public class CAJ_LobbyManager : MonoBehaviourPunCallbacks
         RoomOptions roomOptions = new RoomOptions();
         
         //최대 인원 설정
-        roomOptions.MaxPlayers = 20;
+        //roomOptions.MaxPlayers = 20;
+        roomOptions.MaxPlayers = byte.Parse(inputMaxPlayer.text);
         
         //룸 리스트에 보이게 ? (기본 = true, 공개방)
         roomOptions.IsVisible = true;
         
         //방 생성 요청 (해당 옵션을 이용해서)
-        PhotonNetwork.CreateRoom("MetaMong", roomOptions);
+        PhotonNetwork.CreateRoom(inputRoomName.text, roomOptions);
     }
 
     //2-1-1. 방이 생성되면 호출 되는 함수
@@ -41,13 +71,15 @@ public class CAJ_LobbyManager : MonoBehaviourPunCallbacks
     {
         base.OnCreateRoomFailed(returnCode, message);
         print("OnCreateRoomFailed, " + returnCode + "," + message);
-        JoinRoom();
+        
+        //JoinRoom();
     }
     
     //2-2-1. 방 참가 요청
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom("MetaMong");
+        //PhotonNetwork.JoinRoom("MetaMong");
+        PhotonNetwork.JoinRoom(inputRoomName.text);
     }
     
         
--- Assets/CAJ/Scripts/Player(합칠때 수정)/Billboard.cs
+++ Assets/CAJ/Scripts/Player(합칠때 수정)/Billboard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CAJ_PlayerCam : MonoBehaviour
+public class Billboard : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class CAJ_PlayerCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 닉네임의 앞방향 = 카메라 앞방향으로 셋팅
+        transform.position = Camera.main.transform.forward;
     }
 }
